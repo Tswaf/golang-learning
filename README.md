1e3dd3f1a93120a4029f4eab57dd052f81f84fd9
wangtong <wangtong01@corp.netease.com>
2018-09-03 23:34:18 +0800

    arrays

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Golang学习笔记
 
-- [1.数据类型和变量](doc/golang_types.md)
+- [1.基本数据类型和变量](doc/golang_types.md)
+- [2.数组和切片](/doc/array_and_slice.md)
---
+++ array_and_slice/array.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var a [3]int
+	fmt.Println(a)
+
+	a[0] = 5
+	a[1] = 2
+	a[2] = 6
+	fmt.Println(a)
+
+	b := [2]int{3,8}
+	fmt.Println(b)
+
+	c := [3]int{8}
+	fmt.Println(c)
+
+	d := [...]int{3,4}
+	fmt.Println(d)
+
+	e := [2]string{"cat","dog"}
+	f := e
+	fmt.Println(e,f)
+
+	f[0] = "pig"
+	fmt.Println(e,f)
+
+	letters := [2][3]string{
+		{"a","b","c"},
+		{"A","B","C"},
+	}
+	fmt.Println(letters)
+}
